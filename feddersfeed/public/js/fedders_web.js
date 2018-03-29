c7800aa477ca19a9fa3b7b2eddf2f00d1e8cc3e1
Dave Starke <dave@starkapps.com>
2018-03-29 07:10:34 +0000

    log in page

--- feddersfeed/public/js/fedders_web.js
+++ feddersfeed/public/js/fedders_web.js
@@ -1,12 +1,2 @@
-(function () {
-'use strict';
-
-$(document).ready(function() {
-	$('a[href="#signup"]').css("display","none");
-	$('a[href="#forgot"]').css("color","#C60");
-	$('.page-card-head').after('<div class="logo"></div>');
-
-});
-
-}());
+!function(){"use strict";$(document).ready(function(){$('a[href="#signup"]').css("display","none"),$('a[href="#forgot"]').css("color","#C60"),$(".page-card-head").after('<div class="logo"></div>')})}();
 //# sourceMappingURL=fedders_web.js.map
