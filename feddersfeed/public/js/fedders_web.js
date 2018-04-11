26e07edf62539c0537a2fb19acb3764771104672
Dave Starke <dave@starkapps.com>
2018-04-11 17:32:33 +0000

    login styles

--- feddersfeed/public/js/fedders_web.js
+++ feddersfeed/public/js/fedders_web.js
@@ -1,12 +1,2 @@
-(function () {
-'use strict';
-
-$(document).ready(function() {
-	$('a[href="#signup"]').css("display","none");
-	$('a[href="#forgot"]').css("color","#C60");
-	$('.page-card-head').after('<div class="logo img-responsive"><img src="/assets/custom_styles/images/fedders-logo-barn.svg"/></div>');
-
-});
-
-}());
+!function(){"use strict";$(document).ready(function(){$('a[href="#signup"]').css("display","none"),$('a[href="#forgot"]').css("color","#C60"),$(".page-card-head").after('<div class="logo img-responsive"><img src="/assets/custom_styles/images/fedders-logo-barn.svg"/></div>')})}();
 //# sourceMappingURL=fedders_web.js.map
